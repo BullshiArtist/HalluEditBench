79a3d8e863c768f9d42d04e7032f51254ca9275b
Baixiang Huang <bhuang15@hawk.iit.edu>
2024-10-11 15:05:24 -0500

    organize files

--- code/edit_all_topic.sh
+++ code/edit_all_topic.sh
@@ -1,18 +1,18 @@
 topics=(
-    'art_sculpture'
-    # 'art_sculpture' 'business_brand' 'business_industry' 'business_corporation' 
-    # 'entertainment_anime' 'entertainment_song' 'entertainment_music_genre'
-    # 'geography_glacier' 'geography_volcano' 'geography_forest'
-    # 'health_disease' 'health_symptom' 'health_medication'
-    # 'technology_software' 'technology_programming_language' 'technology_database'
-    # 'event_sport' 'event_history' 'event_film'
-    # 'human_athlete' 'human_writer' 'human_entrepreneur' 'human_scientist'
-    # 'places_country' 'places_city' 'places_landmark'
+    'art_sculpture' 'business_brand' 'business_industry' 'business_corporation' 
+    'entertainment_anime' 'entertainment_song' 'entertainment_music_genre'
+    'geography_glacier' 'geography_volcano' 'geography_forest'
+    'health_disease' 'health_symptom' 'health_medication'
+    'technology_software' 'technology_programming_language' 'technology_database'
+    'event_sport' 'event_history' 'event_film'
+    'human_athlete' 'human_writer' 'human_entrepreneur' 'human_scientist'
+    'places_country' 'places_city' 'places_landmark'
 )
 
 start_time=$(date +%s)
 
-# If you have multiple GPUs, you can run experiments for multiple LLMs in parallel.
+# If you have multiple GPUs, you can run experiments for multiple LLMs in parallel. Specify `--results_dir` 
+# to save the results to a specific directory, otherwise the default directory is where we save the results that we report in the paper.
 for topic in "${topics[@]}"; do
     python3 edit_all_method.py --model_name=llama2-7b --device_edit=0 --device_eval=3 --topic_name="$topic" --results_dir=../tmp &
     python3 edit_all_method.py --model_name=llama3-8b --device_edit=1 --device_eval=3 --topic_name="$topic" --results_dir=../tmp &
--- code/edit_previous_work.sh
+++ code/edit_previous_work.sh
@@ -9,15 +9,9 @@ python run_knowedit_llama2.py --editing_method=IKE --hparams_dir=./hparams/IKE/l
     --data_dir=../data/KnowEdit/benchmark/ZsRE/ZsRE-test-all.json --datatype='zsre' --train_data_path=../data/KnowEdit/benchmark/ZsRE/ZsRE-test-all.json
 
 python run_knowedit_llama2.py --editing_method=MEMIT --hparams_dir=./hparams/MEMIT/llama2-7b \
-    --data_dir=../data/KnowEdit/benchmark/ZsRE/ZsRE-test-all.json --datatype='zsre' --ds_size=2 --metrics_save_dir=../tmp
+    --data_dir=../data/KnowEdit/benchmark/ZsRE/ZsRE-test-all.json --datatype='zsre'
     
 python run_knowedit_llama2.py --editing_method=ROME --hparams_dir=./hparams/ROME/llama2-7b \
-    --data_dir=../data/KnowEdit/benchmark/ZsRE/ZsRE-test-all.json --datatype='zsre' --ds_size=2 --metrics_save_dir=../tmp
-
-python run_knowedit_llama2.py --editing_method=MEND --hparams_dir=./hparams/MEND/llama2-7b \
-    --data_dir=../data/KnowEdit/benchmark/ZsRE/ZsRE-test-all.json --datatype='zsre' --ds_size=2 --metrics_save_dir=../tmp
-    
-python run_knowedit_llama2.py --editing_method=SERAC --hparams_dir=./hparams/SERAC/llama2-7b \
     --data_dir=../data/KnowEdit/benchmark/ZsRE/ZsRE-test-all.json --datatype='zsre'
 
 python run_knowedit_llama2.py --editing_method=LoRA --hparams_dir=./hparams/LoRA/llama2-7b \
@@ -38,13 +32,7 @@ python run_knowedit_llama2.py --editing_method=MEMIT --hparams_dir=./hparams/MEM
     --data_dir=../data/KnowEdit/benchmark/wiki_recent/recent_test.json --datatype='recent'
     
 python run_knowedit_llama2.py --editing_method=ROME --hparams_dir=./hparams/ROME/llama2-7b \
-    --data_dir=../data/KnowEdit/benchmark/wiki_recent/recent_test.json --datatype='recent' --metrics_save_dir=../tmp
-
-python run_knowedit_llama2.py --editing_method=MEND --hparams_dir=./hparams/MEND/llama2-7b \
-    --data_dir=../data/KnowEdit/benchmark/wiki_recent/recent_test.json --datatype='recent' --ds_size=2 --metrics_save_dir=../tmp
-    
-python run_knowedit_llama2.py --editing_method=SERAC --hparams_dir=./hparams/SERAC/llama2-7b \
-    --data_dir=../data/KnowEdit/benchmark/wiki_recent/recent_test.json --datatype='recent' --ds_size=2
+    --data_dir=../data/KnowEdit/benchmark/wiki_recent/recent_test.json --datatype='recent'
 
 python run_knowedit_llama2.py --editing_method=LoRA --hparams_dir=./hparams/LoRA/llama2-7b \
     --data_dir=../data/KnowEdit/benchmark/wiki_recent/recent_test.json --datatype='recent'
@@ -63,12 +51,6 @@ python run_knowedit_llama2.py --editing_method=FT-L --hparams_dir=./hparams/FT-L
 python run_knowedit_llama2.py --editing_method=MEMIT --hparams_dir=./hparams/MEMIT/llama2-7b \
     --data_dir=../data/KnowEdit/benchmark/WikiBio/wikibio-test-all.json --datatype='wikibio'
 
-python run_knowedit_llama2.py --editing_method=MEND --hparams_dir=./hparams/MEND/llama2-7b \
-    --data_dir=../data/KnowEdit/benchmark/WikiBio/wikibio-test-all.json --datatype='wikibio'
-    
-python run_knowedit_llama2.py --editing_method=SERAC --hparams_dir=./hparams/SERAC/llama2-7b \
-    --data_dir=../data/KnowEdit/benchmark/WikiBio/wikibio-test-all.json --datatype='wikibio'
-
 python run_knowedit_llama2.py --editing_method=IKE --hparams_dir=./hparams/IKE/llama2-7b \
     --data_dir=../data/KnowEdit/benchmark/WikiBio/wikibio-test-all.json --datatype='wikibio' --train_data_path=../data/KnowEdit/benchmark/ZsRE/ZsRE-test-all.json
 
@@ -78,7 +60,7 @@ python run_knowedit_llama2.py --editing_method=LoRA --hparams_dir=./hparams/LoRA
 
 // Counter fact
 python run_knowedit_llama2.py --editing_method=ROME --hparams_dir=./hparams/ROME/llama2-7b \
-    --data_dir=../data/KnowEdit/benchmark/wiki_counterfact/test_cf.json --datatype='counterfact' --ds_size=2 --metrics_save_dir=../tmp
+    --data_dir=../data/KnowEdit/benchmark/wiki_counterfact/test_cf.json --datatype='counterfact'
 
 python run_knowedit_llama2.py --editing_method=MEMIT --hparams_dir=./hparams/MEMIT/llama2-7b \
     --data_dir=../data/KnowEdit/benchmark/wiki_counterfact/test_cf.json --datatype='counterfact'
@@ -87,12 +69,6 @@ python run_knowedit_llama2.py --editing_method=FT --hparams_dir=./hparams/FT-M/l
     --data_dir=../data/KnowEdit/benchmark/wiki_counterfact/test_cf.json --datatype='counterfact'
 
 python run_knowedit_llama2.py --editing_method=FT-L --hparams_dir=./hparams/FT-L/llama2-7b \
-    --data_dir=../data/KnowEdit/benchmark/wiki_counterfact/test_cf.json --datatype='counterfact'
-
-python run_knowedit_llama2.py --editing_method=MEND --hparams_dir=./hparams/MEND/llama2-7b \
-    --data_dir=../data/KnowEdit/benchmark/wiki_counterfact/test_cf.json --datatype='counterfact'
-
-python run_knowedit_llama2.py --editing_method=SERAC --hparams_dir=./hparams/SERAC/llama2-7b \
     --data_dir=../data/KnowEdit/benchmark/wiki_counterfact/test_cf.json --datatype='counterfact'
 
 python run_knowedit_llama2.py --editing_method=IKE --hparams_dir=./hparams/IKE/llama2-7b \
@@ -104,26 +80,4 @@ python run_knowedit_llama2.py --editing_method=LoRA --hparams_dir=./hparams/LoRA
 
 
 
-start_time=$(date +%s)
-python edit.py
-end_time=$(date +%s)
-duration1=$((end_time - start_time))
-
-# start_time=$(date +%s)
-# python3 inject_misinfomation.py --editing_method=FT-M --hparams_dir=./hparams/FT-M/llama3-8b --ds_size=100 --metrics_save_dir=../tmp_local_eval
-# end_time=$(date +%s)
-# duration2=$((end_time - start_time))
-
-# start_time=$(date +%s)
-# python3 inject_misinfomation.py --editing_method=ICL --hparams_dir=./hparams/ICL/llama3-8b --ds_size=100 --metrics_save_dir=../tmp_local_eval
-# end_time=$(date +%s)
-# duration3=$((end_time - start_time))
-
-echo "Runtime: $((duration1 / 60)) minutes and $((duration1 % 60)) seconds"
-# echo "Runtime: $((duration2 / 60)) minutes and $((duration2 % 60)) seconds"
-# echo "Runtime: $((duration3 / 60)) minutes and $((duration3 % 60)) seconds"
-
-
-# ROME mistral parallel Runtime: 1 minutes and 53 seconds
-# ROME mistral No parallel Runtime: 1 minutes and 47 seconds
 
